8b897f92eec00b0beeb3993df6e9a6aab7c1aa0f
este6an13 <diquintero@unal.edu.co>
2023-03-19 23:52:04 -0500

    MatrixConstructor returns matrix

--- MatrixConstructor.cpp
+++ MatrixConstructor.cpp
@@ -4,37 +4,39 @@
 
 using namespace std;
 
+vector<vector<double>> BuildMatrix(std::string filename) {
+
+    ifstream infile(filename);
+    int n;
+    infile >> n;
+
+    vector<vector<double>> matrix(n, vector<double>(n));
+
+    int i, j;
+    double c;
+    while (infile >> i >> j >> c) {
+        matrix[i][j] = (i == j) ? 0 : c;
+    }
+
+    return matrix;
+}
+
+
 int main(int argc, char *argv[]) {
+
     if (argc < 2) {
         cout << "Please specify the input file name." << endl;
         return 1;
     }
 
     string filename = argv[1];
-    ifstream input(filename);
-    if (!input.is_open()) {
-        cout << "Failed to open file: " << filename << endl;
-        return 1;
-    }
+    vector<vector<double>> matrix = BuildMatrix(filename);
 
-    int n;
-    input >> n;
-    vector<vector<double>> matrix(n, vector<double>(n));
-
-    int i, j;
-    double c;
-    while (input >> i >> j >> c) {
-        matrix[i][j] = (i == j) ? 0 : c;
-    }
-
-    input.close();
-
-    // print matrix
-    cout << n << endl;
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            cout << i << " " << j << " " << matrix[i][j] << endl;
+    for (int i = 0; i < matrix.size(); i++) {
+        for (int j = 0; j < matrix[i].size(); j++) {
+            std::cout << matrix[i][j] << " ";
         }
+        std::cout << std::endl;
     }
 
     return 0;
