1fbf5b7ffaa73c0ed1c5b0b41f3f9f32d244d87c
este6an13 <diquintero@unal.edu.co>
2023-03-22 23:13:41 -0500

    1-Tree Lower Bound

--- main.cpp
+++ main.cpp
@@ -31,15 +31,42 @@ int main(int argc, char* argv[]) {
 
   pair<vector<int>, double> result = aco.solve(&graph);
 
+  vector<int> path = result.first;  
+  double cost = result.second;
+
   cout << "Best path: ";
-  for (int i = 0; i < result.first.size(); i++) {
-    cout << result.first[i] << " ";
+  for (int i = 0; i < path.size(); i++) {
+    cout << path[i] << " ";
   }
-  cout << "\nBest cost: " << result.second << endl;
+  cout << "\nBest cost: " << cost << endl;
 
   double MSTCost = primMST(matrix);
 
-  cout << "MST cost: " << MSTCost << endl; 
+  cout << "MST Cost (Lower Bound): " << MSTCost << endl; 
+  cout << "MST Performance Rate: " << cost / MSTCost << endl; 
+
+  // Improved Lower Bound with 1-Tree
+  vector<double> one_tree_costs;
+  for (int i = 0; i < matrix.size(); i++) {
+      vector<vector<double>> new_matrix = matrix;
+      new_matrix.erase(new_matrix.begin() + i); // Remove the i-th row    
+      for (int j = 0; j < new_matrix.size(); j++) {
+          new_matrix[j].erase(new_matrix[j].begin() + i); // Removing the i-th column
+      }     
+      double mst_cost = primMST(new_matrix); // MST without ith node
+      vector<double> row_i = matrix[i]; // costs from ith node to every other node
+      row_i.erase(row_i.begin() + i); // diagonal element is excluded since it's 0 always
+      nth_element(row_i.begin(), row_i.begin() + 2, row_i.end()); // 2 first indexes correspond to the smallest elements
+      double closest_sum = row_i[0] + row_i[1]; // we get the two closest distances to node i 
+      mst_cost += closest_sum; // we add this two our mst_cost (1-tree cost)
+
+      one_tree_costs.push_back(mst_cost);
+  }
+
+  double improved_lower_bound = *max_element(one_tree_costs.begin(), one_tree_costs.end()); // max mst cost is improved lower bound
+
+  cout << "Max 1-Tree Cost (Improved Lower Bound): " << improved_lower_bound << endl; 
+  cout << "Max 1-Tree Performance Rate: " << cost / improved_lower_bound << endl; 
 
   return 0;
 }
