706a33a2782df2de7c7155d0e8b191bb8941d044
este6an13 <diquintero@unal.edu.co>
2023-03-26 04:04:31 +0000

    makefile fixed

--- makefile.mk
+++ makefile.mk
@@ -1,23 +1,15 @@
-# Define the compiler and compiler flags
-CXX = g++
-CXXFLAGS = -Wall -Wextra
-
-# Define the names of the source files and the output executable
-SRCS = main.cpp ACO/ACOGraph.cpp ACO/ACO.cpp ACO/Ant.cpp Utils/GraphConstructor.cpp MST/MST.cpp MST/OneTree.cpp
-EXEC = main
-
-# Define the default values for the command line arguments
 NODES ?= 20
 EXPERIMENTS ?= 100
 
-all:
-	# Compile the source files
-	$(CXX) $(CXXFLAGS) $(SRCS) -o $(EXEC)
+cost_distribution_$(NODES)_$(EXPERIMENTS).pdf cost_scatter_plot_$(NODES)_$(EXPERIMENTS).pdf performance_distribution_$(NODES)_$(EXPERIMENTS).pdf solution_$(NODES)_$(EXPERIMENTS).pdf: Scripts/get_results.py Data/costs/costs_$(NODES)_$(EXPERIMENTS).txt Data/graphs/graph_$(NODES)_$(EXPERIMENTS).txt Data/paths/paths_$(NODES)_$(EXPERIMENTS).txt
+	python3 Scripts/get_results.py $(NODES) $(EXPERIMENTS)
 
+Data/costs/costs_$(NODES)_$(EXPERIMENTS).txt Data/graphs/graph_$(NODES)_$(EXPERIMENTS).txt Data/paths/paths_$(NODES)_$(EXPERIMENTS).txt: main
+	./main $(NODES) $(EXPERIMENTS)
+
+.PHONY: run
 run:
-	# Run the executable with the specified parameters
-	./$(EXEC) $(NODES) $(EXPERIMENTS)
+	./main $(NODES) $(EXPERIMENTS)
 
-plot:
-	# Run the Python script with the specified parameters
-	python3 get_results.py $(NODES) $(EXPERIMENTS)
+main: main main.cpp ACO/ACOGraph.cpp ACO/ACO.cpp ACO/Ant.cpp Utils/GraphConstructor.cpp MST/MST.cpp MST/OneTree.cpp
+	g++ -o main main.cpp ACO/ACOGraph.cpp ACO/ACO.cpp ACO/Ant.cpp Utils/GraphConstructor.cpp MST/MST.cpp MST/OneTree.cpp
