8eca9967214510804ce6a63c8f4380e52f238740
este6an13 <diquintero@unal.edu.co>
2023-04-08 23:46:35 +0000

    profiling

---
+++ Profiling/plot_time.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+plt.style.use('Solarize_Light2')
+
+# Set the directory containing the files
+dir_path = './times'
+
+# Define the list of nodes
+nodes = [9, 14, 19, 24, 29, 34, 39, 44, 49]
+
+# Initialize the array to hold the average times
+avg_times = np.zeros(len(nodes))
+
+# Loop over the files and compute the average time for each node count
+for i, node in enumerate(nodes):
+    file_path = os.path.join(dir_path, f"times_{node}_100.txt")
+    times = np.loadtxt(file_path)
+    avg_times[i] = np.mean(times)
+
+# Plot the average times as a function of the number of nodes
+plt.plot(nodes, avg_times, 'o-')
+plt.xlabel('Number of nodes')
+plt.ylabel('Time (seconds)')
+plt.title('Average ACO algorithm running time vs. number of nodes')
+plt.grid(True)
+
+# Save the plot to a PDF file
+plt.savefig('time_vs_nodes.pdf')
