d4e2633152215cc2de410b5cabde5c27098e67a2
este6an13 <diquintero@unal.edu.co>
2023-06-04 16:14:23 -0500

    00 times

--- main00.sh
+++ main00.sh
@@ -2,7 +2,7 @@
 
 EXPERIMENTS=100
 
-for (( NODES = 10; NODES <= 100; NODES += 5 )); do
+for (( NODES = 75; NODES <= 100; NODES += 5 )); do
     echo "Running ./main00 $NODES $EXPERIMENTS"
     ./main00 $NODES $EXPERIMENTS
 done
