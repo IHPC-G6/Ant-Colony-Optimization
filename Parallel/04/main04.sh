4d7b9225ecc9fbb724c124d2e210701f74cfa6ee
este6an13 <diquintero@unal.edu.co>
2023-06-18 13:45:30 -0500

    speed up and efficienciy measures up to np=29

--- Parallel/04/main04.sh
+++ Parallel/04/main04.sh
@@ -4,7 +4,7 @@ NODES=100
 EXPERIMENTS=100
 SEED=42
 
-for (( np = 25; np <= 33; np += 2 )); do
+for (( np = 31; np <= 33; np += 2 )); do
     echo "Running ./main04 $NODES $EXPERIMENTS $SEED"
     mpirun -np "$np" --oversubscribe ./main04 "$NODES" "$EXPERIMENTS" "$SEED"
 done
